321fd1524b1c6ad10691f299d8d96ab6d5b72e3e
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-11-18 15:12:48 +0530

    Update initial db setup script

--- scripts/initial-setup.ts
+++ scripts/initial-setup.ts
@@ -1,12 +1,4 @@
-import * as path from "node:path";
-import { bootstrap } from "@vendure/core";
-import { populate } from "@vendure/core/cli";
 import { config } from "../src/vendure-config"; // Adjust path to your vendure-config
-
-// Define the path to the assets from @vendure/create
-const initialDataPath = require.resolve(
-  "@vendure/create/assets/initial-data.json",
-);
 
 // Ensure synchronize is set to true for this script run
 const populateConfig = {
@@ -16,14 +8,3 @@ const populateConfig = {
     synchronize: true, // This creates the tables on run
   },
 };
-
-populate(() => bootstrap(populateConfig), require(initialDataPath))
-  .then((app) => {
-    console.log("Database schema created and populated with initial data!");
-    return app.close();
-  })
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error("Error during database population:", err);
-    process.exit(1);
-  });
